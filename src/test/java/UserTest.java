c0d6c841db62dfcf326978a37681f8642c8539c6
Erika Rodriguez <rodriguez.eri92@gmail.com>
2024-04-18 17:38:10 -0300

    Add createUser, getUser, getAllUsers, updateUser, deleteUser methods. Assertions with status code

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -8,11 +8,10 @@ import org.solvd.models.User;
 import org.solvd.services.ApiService;
 import org.solvd.services.UserService;
 import org.solvd.utils.Auth;
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import java.util.List;
-
+import java.util.HashMap;
+import java.util.Map;
 import static io.restassured.RestAssured.*;
 
 public class UserTest {
@@ -22,18 +21,17 @@ public class UserTest {
     public static String baseUri="https://gorest.co.in/public/v2";
     @BeforeClass
     public void init(){
-//        userService= new UserService();
         request = new RequestSpecBuilder()
                 .setBaseUri(baseUri)
                 .addHeader("Content-Type", "application/json")
-                .addHeader("Accept", "application/json")
-                .addHeader("Authorizaton", "Bearer "+ Auth.getTokenFromPropertiesFile())
+                .addHeader("Accept", "*/*")
+                .addHeader("Authorization", "Bearer "+ Auth.getTokenFromPropertiesFile())
                 .addFilter(new RequestLoggingFilter())
                 .addFilter(new ResponseLoggingFilter())
                 .build();
     }
 
-    @Test
+    @Test(description = "GET Method")
     public void getAllUsersTest() {
         given().log().all(true).
                 spec(request).
@@ -42,28 +40,44 @@ public class UserTest {
         then().log().all(true).
                 assertThat().
                 statusCode(200);
-//    Response response = userService.getAllUsers();
-//    List<User> userList = response.jsonPath().getList(".", User.class);
-//    Assert.assertEquals(response.statusCode(),200);
-//    //Assert.assertFalse(userList.isEmpty());
     }
-    @Test
+    @Test(description = "GET Method")
     public void getOneUser(){
         given().log().all(true).
                 spec(request).
         when().
-                get("/users/6856561").
+                get("/users/6859132").
         then().log().all(true).
                 assertThat().statusCode(200);
     }
-    @Test
+    @Test(description = "POST Method")
     public void createOneUser(){
         User user = new User("Anna Stuart", "astuart@mail.com","female", "active");
         given().log().all(true).
                 spec(request).
                 body(user).
                 post("/users").
-                then().log().all(true).
+        then().log().all(true).
                 assertThat().statusCode(201);
+    }
+    @Test(description = "PUT Method")
+    public void updateUser(){
+        Map<String,String> map=new HashMap<>();
+        map.put("name", "Mary Stuart");
+        map.put("email", "mstuart@mail.com");
+        given().log().all().
+                spec(request).
+                body(map).
+                patch("/users/6859132").
+                then().log().all(true).
+                assertThat().statusCode(200);
+    }
+    @Test(description = "DELETE Method")
+    public void deleteUser(){
+        given().log().all().
+                spec(request).
+                delete("/users/6859132").
+                then().log().all(true).
+                assertThat().statusCode(204);
     }
 }
