c8fcce1a459a8101463550d87407d10a063fad95
Erika Rodriguez <rodriguez.eri92@gmail.com>
2024-04-24 18:11:12 -0300

    Add delete and update graphql tests

--- src/test/java/GraphqlTest.java
+++ src/test/java/GraphqlTest.java
@@ -1,9 +1,13 @@
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.JsonObject;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.solvd.models.User;
 import org.solvd.utils.Auth;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import com.google.gson.Gson;
 
 import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.RestAssured.given;
@@ -51,10 +55,59 @@ public class GraphqlTest {
     }
     @Test
     public void createAUser(){
+        User userObj= new User("Mary Doe", "mdoe@gmail.com", "female", "active");
+        String jsonObj = new Gson().toJson(userObj);
+        System.out.println(jsonObj);
+        //        JSONPObject createUser= new JSONPObject();
+//        JSONObject createUserVars = new JSONObject();
+        //Create the graphql query in JSON format
+        String query="{\n" +
+                "  \"query\": \"mutation CreateUser { createUser( input: { name: \\\"Erika Smith\\\" gender: \\\"female\\\" email: \\\"esmith@mail.com\\\" status: \\\"active\\\" } ) { user { email gender id name status } }}\"\n" +
+                "}";
+        String bodyQuery="{\n" +
+                "  \"query\": \"mutation CreateUser { createUser( input: { name: \\\"Erika Smith\\\" gender: \\\"female\\\" email: \\\"esmith@mail.com\\\" status: \\\"active\\\" } ) { user { email gender id name status } }}\"\n" +
+                "}";
+
+        Response response = given().log().all()
+                .spec(request)
+                .body(bodyQuery)
+                .when()
+                .post("/graphql");
+
+        response.then().log().all(true).assertThat().statusCode(200);
+    }
+    @Test
+    public void updateUser(){
+        User userObj= new User("Mary Doe", "mdoe@gmail.com", "female", "active");
+        String jsonObj = new Gson().toJson(userObj);
+        System.out.println(jsonObj);
+        //        JSONPObject createUser= new JSONPObject();
+//        JSONObject createUserVars = new JSONObject();
         //Create the graphql query in JSON format
         String bodyQuery="{\n" +
-                "  \"query\": \"mutation CreateUser { createUser(input: null) { clientMutationId }}\"\n" +
+                "  \"query\": \"mutation UpdateUser { updateUser(input: { name: null }) { clientMutationId }}\"\n" +
                 "}";
+
+        Response response = given().log().all()
+                .spec(request)
+                .body(bodyQuery)
+                .when()
+                .post("/graphql");
+
+        response.then().log().all(true).assertThat().statusCode(200);
+    }
+    @Test
+    public void deleteUser(){
+        User userObj= new User("Mary Doe", "mdoe@gmail.com", "female", "active");
+        String jsonObj = new Gson().toJson(userObj);
+        System.out.println(jsonObj);
+        //        JSONPObject createUser= new JSONPObject();
+//        JSONObject createUserVars = new JSONObject();
+        //Create the graphql query in JSON format
+        String bodyQuery="{\n" +
+                "  \"query\": \"mutation DeleteUser { deleteUser(input: { id: null }) { clientMutationId }}\"\n" +
+                "}";
+
         Response response = given().log().all()
                 .spec(request)
                 .body(bodyQuery)
